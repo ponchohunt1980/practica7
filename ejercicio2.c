1aed467b47046540860c3be2c184a2af7d4a6d38
Alfonso De Abiega <ponchohunt1980@gmail.com>
2026-03-26 13:36:04 -0600

    Add ejercicio2.c with conditional statements

---
+++ ejercicio2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    if (0)
+    {
+        printf("Esta instrucción nunca se ejecuta\n");
+        printf("porque la condición siempre es falsa (0).\n"); 
+        
+    }
+    if (-38) 
+        printf("Esta instrucción siempre se ejecuta.\n"); 
+        
+    return 0;
+}
